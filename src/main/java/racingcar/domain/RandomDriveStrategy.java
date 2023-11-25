802f5d8f33f6f453eac08eeb450626a619c6bd87
jinkshower <askthem@naver.com>
2023-11-25 17:58:45 +0900

    feat(domain) :무작위 숫자에 따라 전진거리 결정 기능 구현

---
+++ src/main/java/racingcar/domain/RandomDriveStrategy.java
@@ -0,0 +1,34 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.function.IntSupplier;
+
+public class RandomDriveStrategy implements DriveStrategy {
+
+    private final IntSupplier numberGenerator;
+
+    public RandomDriveStrategy(IntSupplier numberGenerator) {
+        this.numberGenerator = numberGenerator;
+    }
+
+    @Override
+    public int determineDistance() {
+        int generatedNumber = numberGenerator.getAsInt();
+        validateRange(generatedNumber);
+
+        if (generatedNumber >= 4) {
+            return 1;
+        }
+        return 0;
+    }
+
+    private void validateRange(int generatedNumber) {
+        if (generatedNumber < 1 || generatedNumber > 9) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public int generateRandomNumber() {
+        return Randoms.pickNumberInRange(1, 9);
+    }
+}
---
+++ src/test/java/racingcar/domain/RandomDriveStrategyTest.java
@@ -0,0 +1,45 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class RandomDriveStrategyTest {
+
+    @DisplayName("숫자가 4이상 일 때 전진 거리는 1이다")
+    @ParameterizedTest
+    @CsvSource(value = {"4,1", "8,1"})
+    void determineDistance1(int value, int expected) {
+        DriveStrategy randomDriveStrategy = new RandomDriveStrategy(() ->value);
+
+        int actual= randomDriveStrategy.determineDistance();
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("숫자가 4미만 일 때 전진 거리는 0이다")
+    @ParameterizedTest
+    @CsvSource(value = {"1,0", "3,0"})
+    void determineDistance2(int value, int expected) {
+        DriveStrategy randomDriveStrategy = new RandomDriveStrategy(() ->value);
+
+        int actual= randomDriveStrategy.determineDistance();
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("생성된 숫자가 1~9사이가 아니면 예외가 발생 한다")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 10, -1})
+    void thorwException(int value) {
+        DriveStrategy driveStrategy = new RandomDriveStrategy(() -> value);
+
+        assertThatThrownBy(driveStrategy::determineDistance)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
