05e2660de3397bb2acf706b7ea794d5288d00594
jinkshower <askthem@naver.com>
2023-11-26 15:59:09 +0900

    refactor(RacingCars): 자동차 생성후 인스턴스화 하게 변경

--- src/main/java/racingcar/domain/RacingCars.java
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -1,44 +1,31 @@
 package racingcar.domain;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import racingcar.domain.car.Car;
-import racingcar.domain.car.CarBuilder;
-import racingcar.domain.car.CarName;
 
 public class RacingCars {
 
     private static final int MIN_CAR_COUNT = 2;
     private final List<Car> racingCars;
 
-    private RacingCars(List<Car> racingCars) {
-        this.racingCars = new ArrayList<>(racingCars);
+    public RacingCars(List<Car> generatedCars) {
+        validate(generatedCars);
+        this.racingCars = new ArrayList<>(generatedCars);
     }
 
-    public static RacingCars of(String[] splitInput) {
-        validate(splitInput);
-
-        List<Car> generatedCars = new ArrayList<>();
-        for (String input : splitInput) {
-            generatedCars.add(new CarBuilder()
-                    .withCarName(CarName.of(input))
-                    .build());
-        }
-        return new RacingCars(generatedCars);
-    }
-
-    private static void validate(String[] userInput) {
-        if (hasDuplication(userInput)) {
+    private static void validate(List<Car> generatedCars) {
+        if (hasDuplication(generatedCars)) {
             throw new IllegalArgumentException("중복 ㄴㄴ");
         }
-        if (userInput.length < MIN_CAR_COUNT) {
+        if (generatedCars.size() < MIN_CAR_COUNT) {
             throw new IllegalArgumentException("2대이상만");
         }
     }
 
-    private static boolean hasDuplication(String[] text) {
-        return Arrays.stream(text).distinct().count() != text.length;
+    private static boolean hasDuplication(List<Car> generatedCars) {
+        return Set.copyOf(generatedCars).size() != generatedCars.size();
     }
 
     public void moveAll() {
@@ -48,4 +35,16 @@ public class RacingCars {
     public List<Car> getRacingCars() {
         return new ArrayList<>(racingCars);
     }
+
+    //    public static RacingCars of(String[] splitInput) {
+//        validate(splitInput);
+//
+//        List<Car> generatedCars = new ArrayList<>();
+//        for (String input : splitInput) {
+//            generatedCars.add(new CarBuilder()
+//                    .withCarName(CarName.of(input))
+//                    .build());
+//        }
+//        return new RacingCars(generatedCars);
+//    }
 }
